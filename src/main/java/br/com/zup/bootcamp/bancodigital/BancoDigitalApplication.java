1a4387e3ae561a685bc37bd0481ccb9e2a15da8d
João Victor Albuquerque <albjoaov@gmail.com>
2020-10-17 03:07:35 -0300

    feat (setup): init project

---
+++ src/main/java/br/com/zup/bootcamp/bancodigital/BancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.bootcamp.bancodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/bootcamp/bancodigital/BancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.bootcamp.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
