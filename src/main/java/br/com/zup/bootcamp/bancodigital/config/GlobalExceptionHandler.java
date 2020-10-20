560292a8144af319cef11b0873978b8e8ab1b70f
João Victor Albuquerque <albjoaov@gmail.com>
2020-10-20 03:26:26 -0300

    feat (step-three): handling bindException

--- src/main/java/br/com/zup/bootcamp/bancodigital/config/GlobalExceptionHandler.java
+++ src/main/java/br/com/zup/bootcamp/bancodigital/config/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.zup.bootcamp.bancodigital.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,6 +35,18 @@ public class GlobalExceptionHandler {
 	}
 
 	@ResponseStatus (code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler (MethodArgumentNotValidException.class)
+	public List<ApiErrorReturn> handleControllerValidationWithMultipartFormData (BindException exception) {
+
+		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+		return fieldErrors.stream().map(fieldError -> {
+			String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			return new ApiErrorReturn(fieldError.getField(), message);
+		}).collect(Collectors.toList());
+	}
+
+	@ResponseStatus (code = HttpStatus.NOT_FOUND)
 	@ExceptionHandler (javax.persistence.EntityNotFoundException.class)
 	public ApiErrorReturn handleInvalidId (javax.persistence.EntityNotFoundException exception) {
 		return new ApiErrorReturn("id", exception.getMessage());
